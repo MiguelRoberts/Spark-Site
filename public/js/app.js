b9b242aa307e6e70752a758b32402d20e0faa7f3
aakaluza <alekal20@bergen.org>
2020-02-03 13:07:09 -0500

    Changes to the landing page.

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+$(function() {
+    // const $login = $("#login")
+    // const $apply = $("#apply")
+
+    // $login.hover(
+    //     function(){$(this).addClass("hover");},
+    //     function(){$(this).removeClass("hover");}
+    // )
+
+    // $apply.hover(
+    //     function(){$(this).addClass("hover");},
+    //     function(){$(this).removeClass("hover");}
+    // )
+})
