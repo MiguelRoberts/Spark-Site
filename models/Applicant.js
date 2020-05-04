a41b814d0e917e2f8e040a26eb3b21ea9551f31a
Miguel Roberts <miguelaroberts09@gmail.com>
2020-05-04 11:31:02 -0400

    Added Models for Application Process

--- models/Applicant.js
+++ models/Applicant.js
@@ -2,5 +2,18 @@ const mongoose  = require('mongoose')
 const Schema    = mongoose.Schema
 
 const applicantSchema = new Schema({
-    
+    written: {
+        grades: [], // category grades
+        comments: String
+    },
+    individual_interview: {
+        questions: [],
+        questionGrades: [],
+        grades: [], // category grades
+        comments: String
+    },
+    group_interview: {
+        grades: [], // category grades
+        comment: String
+    }
 })
---
+++ models/ApplicationDetails.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const applicationDetailsSchema = new Schema({
+    written: {
+        questions: [], // written questions
+        categories: [] // categories to be graded on
+    },
+    individual_interview: {
+        questions: [], // pool of all questions that can be asked in the interview
+        categories: [] // categories to be graded on
+    },
+    group_interviews: {
+        categories: [] // categories to be graded on
+    }
+})
+
+module.exports = ApplicationDetails = mongoose.model('ApplicationDetails', applicationDetailsSchema)
